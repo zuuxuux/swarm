43ca570863f2a3e5360246c3c30954d383166474
zuuxuux@gmail.com <zuuxuux@gmail.com>
2024-11-19 11:56:39 +0000

    Investigate context variables

---
+++ examples/context_variables.py
@@ -0,0 +1,46 @@
+from swarm import Agent, Swarm
+
+client = Swarm()
+
+
+def instructions(context_variables):
+    name = context_variables.get("name", "User")
+    return f"You are a helpful agent. Greet the user by name ({name})."
+
+
+def print_account_details(context_variables: dict):
+    user_id = context_variables.get("user_id", None)
+    name = context_variables.get("name", None)
+    print(f"Account Details: {name=} {user_id=}")
+    return f"Success: {name=} {user_id=}"
+
+
+agent = Agent(
+    name="Agent",
+    instructions=instructions,
+    functions=[print_account_details],
+)
+
+context_variables = {
+    "name": "James",
+}
+
+response = client.run(
+    messages=[{"role": "user", "content": "Hi!"}],
+    agent=agent,
+    context_variables=context_variables,
+    debug=True,
+)
+print(response.messages[-1]["content"])
+
+response = client.run(
+    messages=[
+        {
+            "role": "user",
+            "content": "Print and explain my account details! Is there anything missing?",
+        }
+    ],
+    agent=agent,
+    context_variables=context_variables,
+)
+print(response.messages[-1]["content"])
